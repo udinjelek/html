1b9b59ba7b4c75cfe835bb83d201b4ca39ae81e9
udin jelek <home.udinjelek@users.noreply.github.com>
2021-02-28 20:39:45 +0700

    home update

---
+++ frontedmentor.io/Newbie/base-apparel-coming-soon-master/main.js
@@ -0,0 +1,15 @@
+function checkInputEmail(){
+	document.getElementById("outputText").className = "redText";
+	document.getElementById("outputText").innerHTML  = "Please provide a valid email"
+	
+	var emailInput = document.getElementById('inputText').value;
+	
+	var partEmail = emailInput.split('@')
+	if( partEmail.length  == 2)
+	{	if( partEmail[1].split('.').length >=2 )
+		{	document.getElementById("outputText").className = "blueText";
+			document.getElementById("outputText").innerHTML  = "Your email has been added, Thanks for Join us!";
+		}
+	}
+
+} 
