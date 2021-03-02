1c2bbe639416e7d8dfc503b30e97f73583092f64
udin jelek <udinjelek@users.noreply.github.com>
2021-03-02 23:06:06 +0700

    work update 2021-03-02

--- frontedmentor.io/Newbie/faq-accordion-card-main/main.js
+++ frontedmentor.io/Newbie/faq-accordion-card-main/main.js
@@ -13,12 +13,26 @@ function hideUnhideAnswer(e){
     // console.log(e.target);  // to get the element
     // console.log(e.target.tagName);  // to get the element tag name alone
     // console.log(e.target.parentNode); // parent Node
-    var nowNodes = e.target.parentNode.childNodes;
+    var nowNodes = null;
+    var arrowNodes = null;
+    
+    if (e.target.tagName === "BUTTON")
+    {   nowNodes = e.target.parentNode.childNodes;
+    } 
+    else
+    {   nowNodes = e.target.parentNode.parentNode.childNodes;
+    }
+    arrowNodes = nowNodes[1].childNodes;
+    
+    // console.log(arrowNodes[1]); // parent Node
     if (    nowNodes[3].style.display === "none") 
     {       nowNodes[3].style.display = "block";
+            arrowNodes[1].className = "arrow";
+            
     }
     else
     {       nowNodes[3].style.display = "none";
+            arrowNodes[1].className = "up";
     }
     
     
